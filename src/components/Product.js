dcd1caa8fabd8d26e4ffba3a2bcd3beeb3b07030
Tarek salah <64745962+tarek-98@users.noreply.github.com>
2024-02-06 15:59:30 +0300

    Add files via upload

--- src/components/Product.js
+++ src/components/Product.js
@@ -326,20 +326,20 @@ function Product({ product, setVideoRef, autoplay }) {
         </div>
       </div>
       <div className={comment ? "com" : "hide-comment"}>
-          <div
-            className="close"
-            onClick={() => setComment((comment) => !comment)}
-          >
-            <IoIosCloseCircleOutline />
-          </div>
-          <div className="comm">
-            <Comments
-              product={product}
-              commentsUrl="http://localhost:3004/comments"
-              currentUserId="1"
-            />
-          </div>
+        <div
+          className="close"
+          onClick={() => setComment((comment) => !comment)}
+        >
+          <IoIosCloseCircleOutline />
         </div>
+        <div className="comm">
+          <Comments
+            product={product}
+            commentsUrl="http://localhost:3004/comments"
+            currentUserId="1"
+          />
+        </div>
+      </div>
     </div>
   );
 }
